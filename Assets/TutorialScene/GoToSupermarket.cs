1a598f0e1033454f0f6019f10875a9595ae342ac
Jasmine Lu <jasminelu.3@gmail.com>
2017-11-22 15:48:06 +0000

    portal stuff

---
+++ Assets/TutorialScene/GoToSupermarket.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToSupermarket : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+        print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+
+    }
+}
