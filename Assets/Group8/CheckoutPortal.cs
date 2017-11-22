e90297e75abd497c876fe82601663d0555415c1d
Galen Han <Mastergalen@users.noreply.github.com>
2017-11-22 15:49:01 +0000

    Create Portal test

---
+++ Assets/Group8/CheckoutPortal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckoutPortal : MonoBehaviour {
+    public GameObject targetPortal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (!c.GetComponent<Rigidbody>()) return;
+
+        int layer = c.gameObject.layer;
+
+        if (layer != LayerMask.NameToLayer("Grabbable")) return;
+
+        Debug.Log("Start Portal collision");
+
+        Teleport(c.gameObject);
+    }
+
+    private void Teleport(GameObject collidingObject)
+    {
+        Rigidbody rb = collidingObject.GetComponent<Rigidbody>();
+
+        collidingObject.transform.position = targetPortal.transform.position;
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+}
