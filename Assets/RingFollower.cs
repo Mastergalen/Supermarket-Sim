75f0d0669bd73cf99586ae8f11fc20b2c7be029d
arvikachristy <anastasia.arvika@gmail.com>
2017-11-22 17:54:41 +0000

    Add ring follower script :hamburger:

---
+++ Assets/RingFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingFollower : MonoBehaviour {
+
+    public GameObject vrCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var newPosition = vrCamera.transform.position;
+
+        newPosition.y -= 0.3f;
+        gameObject.transform.position = newPosition;
+
+	}
+}
