84079be9d8cd9468f5d8bf5564264fa5b4969e18
Galen Han <Mastergalen@users.noreply.github.com>
2017-11-24 16:58:57 +0000

    Add Portal Gun controls

---
+++ Assets/Group8/ControllerMode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMode : MonoBehaviour {
+
+	public enum Mode { Grab, PortalGun };
+	public Mode currentMode;
+
+	private Valve.VR.InteractionSystem.Hand hand;
+
+	void Start () {
+		hand = gameObject.GetComponent<Valve.VR.InteractionSystem.Hand>();
+		currentMode = Mode.Grab;
+	}
+
+	void Update() {
+		if (hand.controller.GetTouchDown ()) {
+			Vector2 touchpad = hand.controller.GetAxis();
+			PortalGun portalGun = GetComponent<PortalGun>();
+
+			if (touchpad.y > 0.7f)
+			{
+				Debug.Log ("Grab Mode");
+				currentMode = Mode.Grab;
+
+				portalGun.Disable();
+			}
+
+			if (touchpad.x > 0.7f)
+			{
+				Debug.Log ("Portal Gun");
+				currentMode = Mode.PortalGun;
+
+				portalGun.Enable();
+			}
+		}
+	}
+}
--- Assets/Group8/Portal/PortalGun/PortalGun.cs
+++ Assets/Group8/Portal/PortalGun/PortalGun.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class PortalGun : MonoBehaviour {
 
+	private Valve.VR.InteractionSystem.Hand hand;
+	private SteamVR_LaserPointer laser;
+
 	// Use this for initialization
 	void Start () {
-		
+		hand = gameObject.GetComponent<Valve.VR.InteractionSystem.Hand>();
+		laser = GetComponent<SteamVR_LaserPointer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ControllerMode.Mode currentMode = GetComponent<ControllerMode> ().currentMode;
+
+		if (currentMode != ControllerMode.Mode.PortalGun) return;
+
+		if (hand.controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			UCL.COMPGV07.Logging.KeyDown();
+			RaycastHit hit;
+
+			if (Physics.Raycast (
+				   hand.controller.transform.pos,
+				   gameObject.transform.forward,
+				   10,
+				   out hit
+			   )) {
+				Debug.Log ("Hit");
+				Debug.Log (hit.point);
+				Debug.DrawLine (hand.controller.transform.pos, hit.point);
+
+				ShootPortal (hit);
+			}
+		}
+	}
+
+	public void Enable() {
+		laser.active = true;
+	}
+
+	public void Disable() {
+		laser.active = false;
+	}
+
+	void ShootPortal(RaycastHit hit) {
+
 	}
 }
