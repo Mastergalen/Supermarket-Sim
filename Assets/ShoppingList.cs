2135ddc8732de47ba513e16a14070c88ef5e8603
Galen Han <Mastergalen@users.noreply.github.com>
2017-11-25 12:34:04 +0000

    Shopping list sticks to hand but in a straight line.

--- Assets/ShoppingList.cs
+++ Assets/ShoppingList.cs
@@ -17,11 +17,12 @@ public class ShoppingList : MonoBehaviour
 
     void Update()
     {
-        items = checkout.GetComponent<Checkout>().experimentManager.ItemsToCollect;
-
         // Using Start () does not work.
         if (!hasSpawned)
         {
+            items = checkout.GetComponent<Checkout>().experimentManager.ItemsToCollect;
+            GameObject[] shoppingList = new GameObject[items.Length];
+            FixedJoint[] joints = new FixedJoint[items.Length];
             foreach (int item in items)
             {
                 // One of the prodcts in the XML has code 56, but there are only 55 items in Inventory
@@ -29,12 +30,18 @@ public class ShoppingList : MonoBehaviour
                 if (index > 54) {
                     index = 54;
                 }
-                Instantiate(
-                    checkout.GetComponent<Checkout>().experimentManager.Inventory[index],
-                    gameObject.transform.position + new Vector3(0, i / 10, 0),
-                    Quaternion.identity
-                );
+                Debug.Log("Spawning product");
+                GameObject product = Instantiate(checkout.GetComponent<Checkout>().experimentManager.Inventory[index], gameObject.transform.position + new Vector3(0, (i * 0.1f), 0), Quaternion.identity);
+                Debug.Log(product);
+
+                product.transform.parent = gameObject.transform;
                 // Change quat.id if rotation is messed up
+                joints[i] = gameObject.AddComponent<FixedJoint>();
+                joints[i].breakForce = 20000;
+                joints[i].breakTorque = 20000;
+                joints[i].connectedBody = product.GetComponent<Rigidbody>();
+
+                shoppingList[i] = product;
                 i++;
             }
             hasSpawned = true;
