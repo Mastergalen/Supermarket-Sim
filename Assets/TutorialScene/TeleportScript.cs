93e42963b84a06a25df14e3c2a99d2cf092eacfb
Jasmine Lu <jasminelu.3@gmail.com>
2017-11-25 00:31:41 +0000

    fixed logic for teleportPoint1 -> teleportPoint2 -> Throwable -> Portal
    portal effect added
    
    set teleportcount back to 0
    
    minor method name changes
    
    rearranged logic

--- Assets/TutorialScene/TeleportScript.cs
+++ Assets/TutorialScene/TeleportScript.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class TeleportScript : MonoBehaviour {
 
-    private GameObject[] teleportAreas;
-    private int teleportCount = 0;
+    public GameObject[] teleportAreas = new GameObject[4];
+    private int tutorialPart = 0;
+
     private GameObject portal;
+    private GameObject throwable;
+    private GameObject food;
 
     // Use this for initialization
     void Start () {
-        teleportAreas = GameObject.FindGameObjectsWithTag("Teleport");
-        portal = GameObject.Find("Portal_01");
-        portal.SetActive(false);
-        SetTeleportStates();
+        InitTutorial();
+        //enable teleport point 1
+        teleportAreas[0].SetActive(true);
     }
 	
 	// Update is called once per frame
@@ -23,27 +27,42 @@ public class TeleportScript : MonoBehaviour {
 
     private void OnTriggerEnter(Collider collisionInfo)
     {
-        if ((collisionInfo.GetComponent<Collider>().name == "HeadCollider") && (teleportCount < teleportAreas.Length - 1))
+        if (collisionInfo.GetComponent<Collider>().name == "HeadCollider") //enable throwables tutorial
         {
-            teleportCount++;
-            SetTeleportStates();
-        } else if ((collisionInfo.GetComponent<Collider>().name == "HeadCollider") && (teleportCount == teleportAreas.Length - 1))
-        {
-            ThrowableTutorial();
-        } else
-        {
-            PortalEffect();
-            Destroy(gameObject.GetComponent<BoxCollider>());
-            Debug.Log(gameObject.GetComponent<BoxCollider>());
-            teleportAreas[teleportCount].SetActive(true);
+            if (tutorialPart == 2)
+            {
+                teleportAreas[2].SetActive(false);
+                food.SetActive(true);
+                throwable.SetActive(true);
+                ThrowableTutorial();
+            }
+            tutorialPart++;
+            SetTargetTeleport();
         }
     }
 
-    private void SetTeleportStates()
+    //disables all teleport points and hides objects that are required for further tutorial parts
+    private void InitTutorial()
+    {
+        foreach (GameObject area in teleportAreas)
+        {
+            area.SetActive(false);
+        }
+        portal = GameObject.Find("Portal_01");
+        portal.SetActive(false);
+
+        throwable = GameObject.Find("Throwables");
+        throwable.SetActive(false);
+
+        food = GameObject.Find("Food");
+        food.SetActive(false);
+    }
+
+    private void SetTargetTeleport()
     {
         for (int i = 0; i < teleportAreas.Length; i++)
         {
-            if (teleportCount == i)
+            if (tutorialPart == i)
             {
                 teleportAreas[i].SetActive(true);
                 gameObject.GetComponent<BoxCollider>().center = new Vector3(teleportAreas[i].transform.position.x, 1.5f, teleportAreas[i].transform.position.z);
@@ -51,20 +70,38 @@ public class TeleportScript : MonoBehaviour {
             {
                 teleportAreas[i].SetActive(false);
             }
-            
         }
-        
     }
 
     private void ThrowableTutorial()
     {
         //TODO tutorial for throwables
-        teleportCount++;
-        SetTeleportStates();
+        ActivatePortal();
     }
 
-    private void PortalEffect()
+    IEnumerator PortalEffect(float time)
     {
-        return;
+        Vector3 originalScale = portal.transform.localScale;
+        Vector3 destinationScale = new Vector3(0.14f, 0.14f, 0.5f);
+
+        float currentTime = 0.0f;
+
+        do
+        {
+            portal.transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
+
+        teleportAreas[tutorialPart].SetActive(true);
+    }
+
+    private void ActivatePortal()
+    {
+        Destroy(gameObject.GetComponent<BoxCollider>());
+        portal.transform.localScale = new Vector3(0, 0, 0);
+        portal.SetActive(true);
+
+        StartCoroutine(PortalEffect(1.5f));        
     }
 }
