8b6dbc9117515559d415d3c676ba865bf1e03888
Galen Han <Mastergalen@users.noreply.github.com>
2017-11-25 14:45:28 +0000

    Shows menu on button press

--- Assets/ShoppingList.cs
+++ Assets/ShoppingList.cs
@@ -5,36 +5,43 @@ using System.Collections.Generic;
 
 public class ShoppingList : MonoBehaviour
 {
-
     public GameObject checkout;
     private int[] items;
-    private int i = 0;
     private bool hasSpawned;
+    private Valve.VR.InteractionSystem.Hand hand;
+    private GameObject[] shoppingList;
 
-    void Start() {
+    void Start()
+    {
+        hand = gameObject.GetComponent<Valve.VR.InteractionSystem.Hand>();
     }
 
     void Update()
     {
-        // While holding button instead of the if below
-        if (!hasSpawned) {
+        if (hand.controller == null) return;
 
+        // While holding button instead of the if below
+        if (!hasSpawned && hand.controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
             items = checkout.GetComponent<Checkout>().experimentManager.ItemsToCollect;
             int numberOfItems = items.Length;
-            GameObject[] shoppingList = new GameObject[numberOfItems];
+            shoppingList = new GameObject[numberOfItems];
 
-            foreach (int item in items) {
-                int index = item - 2;
+            int i = 0;
+
+            foreach (int productCode in items) {
+                int index = productCode - 2;
 
                 GameObject product = Instantiate(
                     checkout.GetComponent<Checkout>().experimentManager.Inventory[index],
                     gameObject.transform.position + new Vector3((i * 0.05f) - ((numberOfItems * 0.05f) / 2) + 0.025f, 0, 0.065f),
-                    Quaternion.Euler(57, 0, 0)
-                    );
+                    Quaternion.identity
+                );
                 Destroy(product.GetComponent<BoxCollider>());
                 Destroy(product.GetComponent<CapsuleCollider>());
                 Destroy(product.GetComponent<Rigidbody>());
+
                 product.transform.parent = transform;
+                product.transform.localRotation = Quaternion.Euler(57, 0, 0);
                 product.transform.localPosition = new Vector3((i * 0.05f) - ((numberOfItems * 0.05f) / 2) + 0.025f, 0, 0.065f);
                 product.transform.localScale -= new Vector3(0.9f, 0.9f, 0.9f);
 
@@ -45,6 +52,11 @@ public class ShoppingList : MonoBehaviour
 
             // destroy objects
             hasSpawned = true;
+        } else if (hasSpawned = hand.controller.GetPressUp(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+            for(int j = 0; j < shoppingList.Length; j++) 
+            {
+                Destroy(shoppingList[j]);
+            }
         }
     }
 }
