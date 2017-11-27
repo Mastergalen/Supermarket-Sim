b2afa93e66b7af1d21bcc8b150db5a1e938886c1
Theo <10600558+theoturner@users.noreply.github.com>
2017-11-27 13:39:54 +0000

    Shopping list centering after items checked out
    Fix bug

--- Assets/ShoppingList.cs
+++ Assets/ShoppingList.cs
@@ -6,12 +6,13 @@ using System.Collections.Generic;
 public class ShoppingList : MonoBehaviour
 {
     public GameObject checkout;
+
+    private Valve.VR.InteractionSystem.Hand hand;
     private GameObject[] inventory;
+    private GameObject[] shoppingList;
     private int[] items;
     private List<int> outstanding;
     private bool hasSpawned;
-    private Valve.VR.InteractionSystem.Hand hand;
-    private GameObject[] shoppingList;
 
     void Start()
     {
@@ -24,20 +25,25 @@ public class ShoppingList : MonoBehaviour
         if (hand.controller == null) return;
 
         // While holding button instead of the if below
-        if (!hasSpawned && hand.controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+        if (!hasSpawned && hand.controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
             UCL.COMPGV07.Logging.KeyDown();
             items = checkout.GetComponent<Checkout>().experimentManager.ItemsToCollect;
             outstanding = checkout.GetComponent<Checkout>().experimentManager.itemsOutstanding;
-            int numberOfItems = items.Length;
+            int numberOfItems = outstanding.Count;
             shoppingList = new GameObject[numberOfItems];
 
             int i = 0;
 
-            foreach (int productCode in items) {
-
-                foreach (GameObject productPrefab in inventory) {
-                    if (productPrefab.GetComponent<ProductCode>().Code == productCode && outstanding.Contains(productCode)) {
-
+            // Go through items to collect
+            foreach (int productCode in items)
+            {
+                // Search through inventory for match
+                foreach (GameObject productPrefab in inventory)
+                {
+                    if (productPrefab.GetComponent<ProductCode>().Code == productCode && outstanding.Contains(productCode))
+                    {
+                        // Spawn outstanding items in shopping list
                         GameObject product = Instantiate(
                             productPrefab,
                             gameObject.transform.position + new Vector3((i * 0.05f) - ((numberOfItems * 0.05f) / 2) + 0.025f, 0, 0.065f),
@@ -47,11 +53,13 @@ public class ShoppingList : MonoBehaviour
                         Destroy(product.GetComponent<CapsuleCollider>());
                         Destroy(product.GetComponent<Rigidbody>());
 
+                        // Attach items to controller position
                         product.transform.parent = transform;
                         product.transform.localRotation = Quaternion.Euler(57, 0, 0);
                         product.transform.localPosition = new Vector3((i * 0.05f) - ((numberOfItems * 0.05f) / 2) + 0.025f, 0, 0.065f);
                         product.transform.localScale -= new Vector3(0.9f, 0.9f, 0.9f);
 
+                        // Store spawned product
                         shoppingList[i] = product;
 
                         i++;
@@ -63,9 +71,12 @@ public class ShoppingList : MonoBehaviour
             // Disable portal mode GUI while showing list
             transform.Find("ControllerGUI").gameObject.SetActive(false);
 
-            // destroy objects
             hasSpawned = true;
-        } else if (hasSpawned = hand.controller.GetPressUp(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+
+        }
+        // Close shopping list when menu button is let go by destroying items
+        else if (hasSpawned = hand.controller.GetPressUp(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
             for(int j = 0; j < shoppingList.Length; j++) 
             {
                 Destroy(shoppingList[j]);
@@ -74,5 +85,6 @@ public class ShoppingList : MonoBehaviour
             // Re-enable controller GUI
             transform.Find("ControllerGUI").gameObject.SetActive(true);
         }
+
     }
 }
